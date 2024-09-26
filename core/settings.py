0ce6c3323932ff022df4e06452cdd8d4bbe08be6
Renan Alves Da Silva <renan.alvescontatos@gmail.com>
2024-09-25 22:29:15 -0300

    correção statics

--- core/settings.py
+++ core/settings.py
@@ -107,9 +107,9 @@ CSRF_TRUSTED_ORIGINS = ['https://gerenciamentos-rio2parking.up.railway.app/']
 
 STATIC_URL = 'static/'
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR,'staticfiles')
+    os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
